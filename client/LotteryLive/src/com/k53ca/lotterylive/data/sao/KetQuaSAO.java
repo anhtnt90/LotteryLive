f6b4155a6b5a9ce4c1db4b563ffe773366f15f53
kingfisherphuoc <phuocdh53@gmail.com>
2013-07-23 18:14:32 +0700

    Abc, doi branch, kho toi quas!

--- client/LotteryLive/src/com/k53ca/lotterylive/data/sao/KetQuaSAO.java
+++ client/LotteryLive/src/com/k53ca/lotterylive/data/sao/KetQuaSAO.java
@@ -1,10 +1,18 @@
 package com.k53ca.lotterylive.data.sao;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.k53ca.lotterylive.data.KetQuaXoSo;
+import com.k53ca.lotterylive.data.sao.KetQuaGsonHelper.KetQuaFromService;
 import com.k53ca.lotterylive.data.sao.KetQuaGsonHelper.KetQuaGson;
 import com.k53ca.lotterylive.utils.Constants.TYPE_XOSO;
 
@@ -20,21 +28,33 @@ public class KetQuaSAO {
 	 * @return {@link KetQuaXoSo} if existed, or return <code>null</code>
 	 */
 	public static KetQuaXoSo getKetQua(TYPE_XOSO type, String date) {
-		// TODO must fix this function later
 		KetQuaXoSo ketQuaXoSo = null;
-		final String ketQuaService = "{\"id\":\"354\","
-				+ "\"date\":\"2011-07-03\","
-				+ "\"result_json\":{\"A\" : \"24644\","
-				+ "\"B\" : \"65344\",\"C\" : \"21119;79826\","
-				+ "\"D\" : \"77925;05173;21183;54571;93080;70089\","
-				+ "\"E\" : \"9457;1409;8051;9987\","
-				+ "\"F\" : \"7266;2780;1213;9698;7595;2367\","
-				+ "\"G\" : \"212;207;977\"," + "\"H\" : \"70;97;23;48\"}}";
-		Log.e("TEST", ketQuaService);
-		Gson gson = new Gson();
+		// TODO: service connection, later use, don't delete it
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair(ServiceConfig.TAG_TYPE, "1"));
+		params.add(new BasicNameValuePair(ServiceConfig.TAG_DATE, date));
+
+		// Ket qua parsed from json
+		String respond = "";
 		try {
-			KetQuaGson ketQuaGson = gson.fromJson(ketQuaService,
-					KetQuaGson.class);
+			switch (type) {
+			case MIEN_BAC:
+				respond = ServiceHelper.getInfo(
+						ServiceConfig.URL_GET_KET_QUA_XO_SO_MIEN_BAC, params,
+						null, 15000);
+				Log.e("TEST", respond);
+				break;
+
+			default:
+				break;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			Gson gson = new Gson();
+			KetQuaGson ketQuaGson = gson.fromJson(respond, KetQuaGson.class);
 			ketQuaXoSo = new KetQuaXoSo();
 			ketQuaXoSo.setDate(ketQuaGson.date);
 			ketQuaXoSo.setServiceId(ketQuaGson.id);
@@ -53,5 +73,4 @@ public class KetQuaSAO {
 		}
 		return ketQuaXoSo;
 	}
-
 }
---
+++ client/LotteryLive/src/com/k53ca/lotterylive/data/sao/ServiceConfig.java
@@ -0,0 +1,16 @@
+package com.k53ca.lotterylive.data.sao;
+
+public class ServiceConfig {
+	// ========================================================================
+	// URL link of get ket qua xo so
+	// ========================================================================
+	private static final String URL_XOSO = "http://www.banvemaybayonline.vn/xs/xo_so/";
+	public static final String URL_GET_KET_QUA_XO_SO_MIEN_BAC = URL_XOSO
+			+ "mienbac";
+
+	// ========================================================================
+	// Params to use with url above
+	// ========================================================================
+	public static final String TAG_TYPE = "type";
+	public static final String TAG_DATE = "date";
+}
